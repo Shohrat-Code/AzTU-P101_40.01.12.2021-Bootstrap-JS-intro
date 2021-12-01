d52b363e10809cb152464a1b0a5af7b175534f69
Shohrat-Code <shohrat@code.edu.az>
2021-12-01 12:30:25 +0400

    Codes

---
+++ js/script.js
@@ -0,0 +1,125 @@
+// "use strict"
+// function First() {
+//     console.log("Test function called!");
+// }
+
+// var a = 15;
+// console.log(typeof a);
+
+// let b = 15;
+// console.log(typeof b);
+
+// b = true;
+// console.log(typeof b);
+
+// const pi = 3.14;
+
+// let num1 = 15;
+// let num2 = 20n;
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+
+// let name = "Abbas";
+// console.log(typeof name);
+
+// let ələkbər = "Test";
+// console.log(ələkbər);
+
+// let a = 15;
+// a = 20;
+
+// console.log(a);
+
+// alert("Lorem ipsum");
+// let num1 = prompt("Enter the number:");
+// console.log(num1);
+
+// if (confirm("Are you sure?") == true) {
+//     console.log("Deleted");
+// } else {
+//     console.log("Not Deleted");
+// }
+
+
+// let a = "15";
+// let b = 15;
+// console.log(a == b);
+// console.log(a === b);
+
+// let num1 = Number(prompt("Enter the number 1:"));
+// let num2 = Number(prompt("Enter the number 2:"));
+// // console.log(num1 + num2);
+
+// if (num1 == 50 || num2 == 50 || (num1 + num2) == 50) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+
+
+// let a = "25";
+// let b = 15;
+// console.log(a + b);
+// console.log(a - b);
+
+// let name1 = "Abbas";
+// let name2 = "Asim";
+
+// console.log(name1 == name2);
+
+// if (15 > 20) {
+//     console.log(true);
+// } else if (15 == 20) {
+//     console.log("equale");
+// } else {
+//     console.log(false);
+// }
+
+// let season = prompt("Enter the season:");
+
+// switch (season) {
+//     case "yaz":
+//         console.log("yaz");
+//         break;
+//     case "yay":
+//         console.log("yay");
+//         break;
+//     case "payiz":
+//         console.log("payiz");
+//         break;
+//     case "qis":
+//         console.log("qis");
+//         break;
+//     default:
+//         console.log("Enter the correct season!");
+//         break;
+// }
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let i = 20;
+
+// while (i < 10) {
+//     console.log("While " + i);
+//     i++;
+// }
+
+// do {
+//     console.log("Do-While " + i);
+//     i++;
+// } while (i < 10);
+
+let a = null;
+
+// if (a == null) {
+//     console.log("null");
+// } else {
+//     console.log("Ok");
+// }
+
+// console.log(a ?? "Ok");
+// console.log(a == null ? "Null" : "Ok");
